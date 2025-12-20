a76a090a1487dc10261995ac059282c05a47b9d3
Ethan Zhang <ethanz4@illinois.edu>
2025-12-19 18:49:50 -0600

    lockfile support
    An annoying part of the current application is that a new instance is
    spawned every time I push the keybind, even if an instance is already
    running. This change fixes that.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::fs::File;
+use std::io;
+use std::path::{Path, PathBuf};
+
+fn make_lockfile() -> io::Result<PathBuf> {
+    use std::env;
+
+    let mut lock_path = env::var("TMPDIR").map_or(PathBuf::from("/tmp"), |dir| {
+        let tentative_dir = Path::new(&dir);
+        if tentative_dir.is_dir() {
+            PathBuf::from(tentative_dir)
+        } else {
+            PathBuf::from("/tmp")
+        }
+    });
+
+    lock_path.push("sway-easyfocus-lockfile");
+    File::create_new(&lock_path).map(move |_| lock_path)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let lock_path = make_lockfile().expect("Lockfile exists! (Is another instance running?)");
+
+    std::thread::sleep(std::time::Duration::from_secs(10));
+
+    std::fs::remove_file(&lock_path).expect(&format!(
+        "Failed to remove lockfile at {}.",
+        &lock_path.display()
+    ));
 }
